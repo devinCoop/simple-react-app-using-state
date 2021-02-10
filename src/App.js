b6ec86bdf9c781835736ba29bb1dd82970285354
Devin Cooper <devinraycooper@gmail.com>
2021-02-09 20:29:05 -0500

    simple react app using state

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MyList from "./MyList";
+import MyForm from "./MyForm";
+
+function App() {
+  return <MyForm />;
+}
+
+export default App;
